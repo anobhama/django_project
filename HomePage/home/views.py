838213070caf13e23bba1220ec13ca1263158f8c
Anobhama <anobhama99@gmail.com>
2020-09-18 12:08:46 +0530

    add of 2 nums in home page

--- HomePage/home/views.py
+++ HomePage/home/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,"home.html",{'name':'Anobhama'})
+
+def sum(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    final = val1 + val2 
+    return render(request,'result.html', {'result': final})
