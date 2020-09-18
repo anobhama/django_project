a23bf4319ec775db88ac35cc38a0032bd2c4d5c9
Anobhama <anobhama99@gmail.com>
2020-09-19 00:13:24 +0530

    making GET to POST

--- HomePage/home/views.py
+++ HomePage/home/views.py
@@ -6,7 +6,7 @@ def home(request):
     return render(request,"home.html",{'name':'Anobhama'})
 
 def sum(request):
-    val1 = int(request.GET['num1'])
-    val2 = int(request.GET['num2'])
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
     final = val1 + val2 
     return render(request,'result.html', {'result': final})
